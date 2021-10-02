fd0372140a04b9f7e1b35df3f180656c764c27f4
javi-santex <84360777+javi-santex@users.noreply.github.com>
2021-10-02 20:51:37 -0300

    xd

--- download_report.py
+++ download_report.py
@@ -1,10 +1,12 @@
 import requests
 import time
+import pathlib
 # Reporte COVID-19
 report_url = 'https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/reporte-covid/dataset_reporte_covid_sitio_gobierno.csv'
 
 print("DOWNLOADING...")
-time.sleep(60)
 r = requests.get(report_url)
-with open('./covid_report.csv', 'wb') as f:
+dir = '{}/covid_report.csv'.format(pathlib.Path(__file__).parent.resolve())
+with open(dir, 'wb') as f:
     f.write(r.content)
+time.sleep(60)
