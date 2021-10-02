fcf1daa506f6ef1d10e802172439c67c5f73317b
javi-santex <84360777+javi-santex@users.noreply.github.com>
2021-10-02 20:38:54 -0300

    anda por favor

--- download_report.py
+++ download_report.py
@@ -1,7 +1,7 @@
 import requests
 import time
 # Reporte COVID-19
-report_url = 'https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/reporte-covid/dataset_reporte_covid_sitio_gobierno.xlsx'
+report_url = 'https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/reporte-covid/dataset_reporte_covid_sitio_gobierno.csv'
 
 print("DOWNLOADING...")
 time.sleep(60)
