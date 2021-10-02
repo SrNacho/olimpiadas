5a32d7c39fb6eb0b02371564a21b876f5105230b
javi-santex <84360777+javi-santex@users.noreply.github.com>
2021-10-02 20:34:13 -0300

    xd

--- download_report.py
+++ download_report.py
@@ -3,9 +3,8 @@ import time
 # Reporte COVID-19
 report_url = 'https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/reporte-covid/dataset_reporte_covid_sitio_gobierno.xlsx'
 
-while True:
-  print("DOWNLOADING...")
-  time.sleep(60)
-  r = requests.get(report_url)
-  with open('./covid_report.xlsx', 'wb') as f:
-      f.write(r.content)
+print("DOWNLOADING...")
+time.sleep(60)
+r = requests.get(report_url)
+with open('./covid_report.csv', 'wb') as f:
+    f.write(r.content)
