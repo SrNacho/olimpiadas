4e1440e01486487043f8b686c173187c38956f20
SrNacho <fotosydibujos@hotmail.com>
2021-10-03 22:28:47 -0300

    new version

--- sitio/views.py
+++ sitio/views.py
@@ -3,6 +3,21 @@ import pandas as pd
 from datetime import datetime
 import moment
 from time import process_time
+from pandas.core.frame import DataFrame
+import requests
+
+# Reporte COVID-19
+report_url = 'https://cdn.buenosaires.gob.ar/datosabiertos/datasets/salud/reporte-covid/dataset_reporte_covid_sitio_gobierno.csv'
+
+def get_new_report():
+    print("DOWNLOADING...")
+    r = requests.get(report_url)
+    covid_report = r.content.decode()
+    df = pd.DataFrame([x.split(',') for x in covid_report.split('\n')])
+    df = df[(len(df.index)-5312):-1]
+    df.columns = ['FECHA','TIPO_REPORTE','TIPO_DATO','SUBTIPO_DATO','VALOR','FECHA_PROCESO','ID_CARGA']
+    df.index.name = None
+    return df
 
 def custom_date_parser(date):
     t1_start = process_time()
@@ -14,25 +29,17 @@ def custom_date_parser(date):
     print("Con for: ", t1_stop-t1_start)
     return x
 
-
 def date_formatter(date):
     return [datetime.strftime(moment.date(row['FECHA'][0:9]).date, '%Y-%m-%d') for index, row in date.iterrows()]
 
-
 def df_state():
-    line = 0
-    chksz = 1000
-    for chunk in pd.read_csv("covid_report.csv", chunksize=chksz, index_col=0):line += chunk.shape[0]
-    df = pd.read_csv('covid_report.csv', skiprows=line-5311, names=[
-                     'FECHA', 'TIPO_REPORTE', 'TIPO_DATO', 'SUBTIPO_DATO', 'VALOR', 'FECHA_PROCESO', 'ID_CARGA'])
-    df.to_csv('./new_covid_report.csv', index=None)  # With less lines (5311)
-    new_df = pd.read_csv('new_covid_report.csv')
-    dates_from_df = new_df['FECHA'].to_frame()
+    df = get_new_report()
+    dates_from_df = df['FECHA'].to_frame()
     new_dates_from_df = date_formatter(dates_from_df)
     new_dates_from_df = pd.DataFrame(new_dates_from_df, columns=['FECHA'])
-    df['FECHA'] = new_dates_from_df['FECHA']
+    df['FECHA'] = new_dates_from_df['FECHA'].values
+    print(df)
     df = df.sort_values(by=["FECHA"], ascending=False)
-
     global dataFrame
     dataFrame = df
 
@@ -41,7 +48,12 @@ flag = 0
 def index(request):
     global flag
     print(flag)
+    t1_start = process_time() 
     if flag == 0:
         flag = 1
         df_state()
+    t1_stop = process_time()
+    print("Elapsed time:", t1_stop, t1_start) 
+    print("Elapsed time during the whole program in seconds:",
+                                         t1_stop-t1_start) 
     return JsonResponse(dataFrame.to_dict('records'), safe=False)
